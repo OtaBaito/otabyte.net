7da4c480795c33282a5927c767c1623770fbaa04
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2022-05-29 00:23:41 +0700

    Project Setup

---
+++ components/Maintenance.vue
@@ -0,0 +1,47 @@
+<!-- Please remove this file from your project -->
+<template>
+	<div class="flex flex-col lg:flex-row justify-center items-center">
+		<img
+			class="h-56 lg:h-screen w-full lg:w-1/2 object-cover"
+			src="https://otabyte.net/ota.webp"
+			alt="Akihabara Illustration"
+		/>
+		<div class="lg:w-1/2 pt-6 px-4 sm:px-6 lg:px-8 px-4">
+			<h1
+				class="
+					text-4xl
+					tracking-tight
+					font-extrabold
+					text-gray-900
+					sm:text-5xl
+					md:text-6xl
+				"
+			>
+				Coming Soon
+			</h1>
+			<p
+				class="
+					lg:w-3/4
+					text-justify
+					mt-3
+					text-base text-gray-500
+					sm:text-lg
+					md:text-xl
+				"
+			>
+				We tried our best to ensure refined quality website for you with
+				our small group's tiny labs and deep research, to make sure you
+				had the best experiences while surfing on otabyte.
+			</p>
+			<span class="absolute bottom-0 lg:right-0 p-4 text-gray-900"
+				>Copyright &copy; 2021 OtaByte All Rights Reserved.</span
+			>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'OtaMaintenance',
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+export default {
+	// Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+	ssr: false,
+
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
+
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'OtaByte',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ name: 'format-detection', content: 'telephone=no' },
+
+			{ hid: 'description', name: 'description', content: 'We offer high-quality content for everyone to watch anime and read manga, bookmark where you leave your chapters and episode with our tracker.' },
+			{ name: 'image', content: 'https://otabyte.net/ota.webp' },
+
+			{ property: "og:type", content: "website" },
+			{ property: "og:url", content: "https://otabyte.net/" },
+			{ property: "og:title", content: "OtaByte - Anime Everytime" },
+			{ property: "og:description", content: "We offer high-quality content for everyone to watch anime and read manga, bookmark where you leave your chapters and episode with our tracker." },
+			{ property: "og:image", content: "https://otabyte.net/ota.webp" },
+
+			{ property: "fb:app_id", content: "259805548002889" },
+
+			{ property: "twitter:card", content: "summary_large_image" },
+			{ property: "twitter:url", content: "https://otabyte.net/" },
+			{ property: "twitter:title", content: "OtaByte - Anime Everytime" },
+			{ property: "twitter:description", content: "We offer high-quality content for everyone to watch anime and read manga, bookmark where you leave your chapters and episode with our tracker." },
+			{ property: "twitter:image", content: "https://otabyte.net/ota.webp" },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/eslint
+		'@nuxtjs/eslint-module',
+		// https://go.nuxtjs.dev/tailwindcss
+		'@nuxtjs/tailwindcss',
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+		// https://go.nuxtjs.dev/pwa
+		'@nuxtjs/pwa',
+		// https://go.nuxtjs.dev/content
+		'@nuxt/content',
+	],
+
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {
+		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+		baseURL: '/',
+	},
+
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+			lang: 'en',
+		},
+	},
+
+	// Content module configuration: https://go.nuxtjs.dev/config-content
+	content: {},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+}
